a8c801a5b39f75a0aa7da95a7b004ceede62dbb7
mahfuzar175 <mahfuzar175@gmail.com>
2023-12-22 16:36:40 +0600

    firebase deployed

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosSecure = axios.create({
-    baseURL: "http://localhost:5000"
+    baseURL: "https://simple-landing-page-server-side.vercel.app"
 })
 const useAxiosSecure = () => {
     return axiosSecure;
